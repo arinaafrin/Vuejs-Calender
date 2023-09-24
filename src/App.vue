3cf4c9cb2ee7e8514f0c198f5f2eb3f4772aac59
mitu2964 <arinaafrin05@gmail.com>
2023-09-24 11:12:44 +0600

    Vue js calendar created successfully

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader />
+  <div class="w-full flex">
+    <!-- <DcHeros /> -->
+    <Calendar />
+  </div>
+</template>
+
+<script>
+  import AppHeader from "./components/AppHeader.vue";
+  import Calendar from "./components/Calendar.vue";
+  // import DcHeros from "./components/DcHeros.vue";
+  export default {
+      components: { AppHeader, Calendar }
+  };
+</script>
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav
+      class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-5"
+    >
+      <router-link class="mr-2" to="/">Home</router-link>
+      <router-link class="mr-2" to="/">About Us</router-link>
+      <router-link class="" to="/">Contact</router-link>
+      <router-link
+        key=""
+        class="mx-2 hidden md:inline-block"
+        to=""
+        ></router-link
+      >
+      <div class="float-right">
+        <button class="mx-2" @click="logout">Logout</button>
+        <button  class="mx-2">
+            Login
+        </button>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+
+  </script>
+  
+  <style></style>
